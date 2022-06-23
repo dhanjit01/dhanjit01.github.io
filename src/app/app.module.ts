262f68153151103bece4900c452c1a36c64d8f94
nabajit00 <unabajit321@gmail.com>
2022-06-24 01:07:43 +0530

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {GalleriaModule} from 'primeng/galleria';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GalleriaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
